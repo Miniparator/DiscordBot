86f414e357ee59a53956f7dd7b6d95aaecc591f8
Minii <steffen.begemann@web.de>
2020-02-19 21:08:07 +0100

    Kommentieren und Rechencommand hinzufuegen

--- DiscordBot.py
+++ DiscordBot.py
@@ -7,19 +7,30 @@ class MyBot(discord.Client):
         
     #Wenn Nachricht empfangen wird
     async def on_message(self, message):
+        message_content = message.content
+
+        #Wenn die Nachricht vom Bot selber ist
         if message.author == bot.user:
             return
         
         if str(message.channel) == "bottest1":
-            await message.channel.send("Auch hi")
-    
+            if message_content[0] == "/":
+                command = message_content.split("/")[1].split(" ")
+                answer = ""
+                if command[0] == "solve":
+                    answer = eval(command[1]) 
+                
+                await message.channel.send(answer)
+
+   
+#Token aud Datei auslesen die Nicht auf Github hochgeladen wird 
 file = open("key.txt")
 file_content = file.read()
 file.close()
 
+#Das \n entfernen
 key = file_content.rstrip()
 
-print(key)
-    
+#Bot starten
 bot = MyBot()
 bot.run(key)
